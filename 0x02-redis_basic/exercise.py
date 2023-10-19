51e595d1847e3a499eaaeca623d06d36d573c020
Erickkaranja <karanja99erick@gmail.com>
2023-10-19 10:00:30 +0300

    updating call_history function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,31 @@ from typing import Union, Callable
 import uuid
 import pickle
 
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def call_history(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs):
+        # Get the qualified name of the decorated function
+        function_name = f"{method.__qualname__}"
+        print(function_name)
+
+        # Create keys for input and output lists
+        input_key = f"{function_name}:inputs"
+        output_key = f"{function_name}:outputs"
+
+        # Append input arguments to the input list
+        input_data = str(args)
+        r.rpush(input_key, input_data)
+
+        # Execute the original function and store its output
+        result = method(*args, **kwargs)
+        output_data = pickle.dumps(result)  # Serialize the output to store in Redis
+        r.rpush(output_key, output_data)
+
+        return result
+
+    return wrapper
 
 def count_calls(method: Callable):
     '''function that counts the numder of times a given function is called.'''
@@ -29,6 +54,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''The method should generate a random key (e.g. using uuid).
