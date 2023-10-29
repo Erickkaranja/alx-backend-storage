2d8a0635e005914b7093e149a9301689a25a6138
Erickkaranja <karanja99erick@gmail.com>
2023-10-29 23:23:25 +0300

    updating docs

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,20 +8,17 @@ BEGIN
     DECLARE total_corrections INT;
     DECLARE avg_score FLOAT;
 
-    -- Calculate the total score and the total number of corrections for the user
     SELECT SUM(score), COUNT(*) 
     INTO total_score, total_corrections
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Calculate the average score (handle division by zero)
     IF total_corrections > 0 THEN
         SET avg_score = total_score / total_corrections;
     ELSE
         SET avg_score = 0;
     END IF;
 
-    -- Update the user's average score in the users table
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
