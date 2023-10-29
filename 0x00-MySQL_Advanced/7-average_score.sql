0bb59fcd87f924e343998e6eb063a3f2d960977c
Erickkaranja <karanja99erick@gmail.com>
2023-10-29 23:17:12 +0300

    updating task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,28 @@
-DELIMITER //
+DELIMITER $$
 
--- Create a stored procedure to compute and store the average score for a user
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average DECIMAL(10, 2);
-    
-    -- Compute the average score for the user
-    SELECT AVG(score) INTO average
-    FROM scores
+    DECLARE total_score FLOAT;
+    DECLARE total_corrections INT;
+    DECLARE avg_score FLOAT;
+
+    -- Calculate the total score and the total number of corrections for the user
+    SELECT SUM(score), COUNT(*) 
+    INTO total_score, total_corrections
+    FROM corrections
     WHERE user_id = user_id;
+
+    -- Calculate the average score (handle division by zero)
+    IF total_corrections > 0 THEN
+        SET avg_score = total_score / total_corrections;
+    ELSE
+        SET avg_score = 0;
+    END IF;
 
     -- Update the user's average score in the users table
     UPDATE users
-    SET average_score = average
+    SET average_score = avg_score
     WHERE id = user_id;
-END;
-//
-DELIMITER ;
+END $$
 
+DELIMITER ;
